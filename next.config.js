7cd8cfd76d03c9b9185259c30d0ce4ff64389cd4
Kim Tang <kimtang@192-168-1-102.tpgi.com.au>
2022-12-30 01:39:14 +1100

    build: set up storybook

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  resolve: {
+    fallback: {
+      util: true,
+      // other fallback options...
+    },
+  },
 };
 
 module.exports = nextConfig;
